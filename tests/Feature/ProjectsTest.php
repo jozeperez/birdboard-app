3f9e84af3aded026f954483c642cb21b9537f834
Joze Perez <jozeperezp@gmail.com>
2020-05-22 06:56:56 -0400

    Created two more test for our ProjectsController: ensure we require title; ensure we require description. Also refactored @store method a little bit.

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -10,19 +10,30 @@ class ProjectsTest extends TestCase
 {
     use withFaker, RefreshDatabase;
 
-    public function testAUserCanCreateAProject()
+    public function test_a_user_can_create_a_project()
     {
         $this->withoutExceptionHandling();
 
-        $attributes = [
-            'title'       => $this->faker->sentence,
-            'description' => $this->faker->paragraph
-        ];
+        $attributes = factory('App\Project')->raw();
 
         $this->post('/projects', $attributes)->assertRedirect('/projects');
 
         $this->assertDatabaseHas('projects', $attributes);
 
         $this->get('/projects')->assertSee($attributes['title']);
+    }
+
+    public function test_a_project_requires_a_title()
+    {
+        $attributes = factory('App\Project')->raw(['title' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
+    }
+
+    public function test_a_project_requires_a_description()
+    {
+        $attributes = factory('App\Project')->raw(['description' => '']);
+
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 }
