20587bded319f614d79337c3667329245adc4ebb
Joze Perez <jozeperezp@gmail.com>
2020-05-28 07:06:59 -0400

    Enhanced UI. Removed bootstrap support and cleaned up a lot of OTB laravel html/css. Began buidling out projects dashboard using TailwindCss.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,19 @@
 @extends ('layouts.app')
 
 @section ('content')
-    <div class="flex items-center mb-4">
-        <h1 class="mr-auto">BirdBoard App</h1>
+    <div class="flex items-center mb-3">
         <a href="{{ route('create-project') }}">New Project</a>
     </div>
 
-    <ul>
+    <div class="flex">
         @forelse ($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">
-                    {{ $project->title }}
-                </a>
-            </li>
+            <div class="bg-white mr-4 rounded p-5 shadow w-1/3" style="height: 300px;">
+                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+
+                <div class="text-gray-500">{{ Str::limit($project->description, 150) }}</div>
+            </div>
         @empty
-            <li>No projects yet.</li>
+            <div>No projects yet.</div>
         @endforelse
-    </ul>
+    </div>
 @endsection
