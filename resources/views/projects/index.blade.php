d1f6c98e27fc897c8979e376696cfd3c9f1ae4d6
Joze Perez <jozeperezp@gmail.com>
2020-05-29 07:36:02 -0400

    UI Beautification: Worked on styling the project dashboard page.

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,19 +1,29 @@
 @extends ('layouts.app')
 
 @section ('content')
-    <div class="flex items-center mb-3">
-        <a href="{{ route('create-project') }}">New Project</a>
-    </div>
+    <header class="flex items-center mb-3 py-4">
+        <div class="flex justify-between items-center w-full">
+            <h2 class="text-gray-500 text-sm">My Projects</h2>
 
-    <div class="flex">
+            <a href="{{ route('create-project') }}" class="button">New Project</a>
+        </div>
+    </header>
+
+    <main class="lg:flex lg:flex-wrap -mx-3">
         @forelse ($projects as $project)
-            <div class="bg-white mr-4 rounded p-5 shadow w-1/3" style="height: 300px;">
-                <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+            <div class="lg:w-1/3 px-3 pb-6">
+                <div class="bg-white rounded-lg p-5 shadow" style="height: 300px;">
+                    <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-300 pl-4">
+                         <a href="{{ $project->path() }}" class="text-black no-underline">
+                            {{ $project->title }}
+                        </a>
+                    </h3>
 
-                <div class="text-gray-500">{{ Str::limit($project->description, 150) }}</div>
+                    <div class="text-gray-500">{{ Str::limit($project->description, 150) }}</div>
+                </div>
             </div>
         @empty
             <div>No projects yet.</div>
         @endforelse
-    </div>
+    </main>
 @endsection
