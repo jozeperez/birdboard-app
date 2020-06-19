898f9dbffa9e5a749f30a034d4e89d2a4140bb14
Joze Perez <jozeperezp@gmail.com>
2020-06-19 07:05:24 -0400

    Added Activity feed to the project UI. Have it displaying action with how long ago it was executed. Still have room for improving the UI.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -82,6 +82,7 @@
 
             <div class="lg:w-1/4 px-3">
                 @include ('projects.card')
+                @include ('projects.activity.card')
             </div>
         </div>
     </main>
