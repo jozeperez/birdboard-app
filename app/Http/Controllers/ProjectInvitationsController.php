ad9903740163211e43b4c2ef7921bcbb659d13e4
Joze Perez <jozeperezp@gmail.com>
2020-07-15 07:25:28 -0400

    Refactored store() of the projects invitations controller to now leverage a form request.

--- app/Http/Controllers/ProjectInvitationsController.php
+++ app/Http/Controllers/ProjectInvitationsController.php
@@ -2,22 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProjectInvitationRequest;
 use App\Project;
 use App\User;
-use Illuminate\Http\Request;
 
 class ProjectInvitationsController extends Controller
 {
-    public function store(Project $project)
+    public function store(Project $project, ProjectInvitationRequest $request)
     {
-        $this->authorize('update', $project);
-
-        request()->validate([
-            'email' => ['required', 'exists:users,email']
-        ], [
-            'email.exists' => 'The user you are inviting must have a Birdboard account.'
-        ]);
-
         $user = User::whereEmail(request('email'))->first();
 
         $project->invite($user);
