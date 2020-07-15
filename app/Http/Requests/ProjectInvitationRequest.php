ed0ffbc9a3e52587f0ff55b8eb19dc2a1989e42c
Joze Perez <jozeperezp@gmail.com>
2020-07-15 08:05:25 -0400

    Modified ProjectInvitationRequest to leverage the Illuminate\Validation\Rule class when checking if user - matching by email address.

--- app/Http/Requests/ProjectInvitationRequest.php
+++ app/Http/Requests/ProjectInvitationRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rule;
 
 class ProjectInvitationRequest extends FormRequest
 {
@@ -25,7 +26,7 @@ class ProjectInvitationRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => ['required', 'exists:users,email']
+            'email' => ['required', Rule::exists('users', 'email')]
         ];
     }
 
